310c84bfb1b596b920efa21f0f54765c111ce67a
Drumsimus <danila.yashin23@gmail.com>
2024-04-27 16:33:52 +0300

    update classifiers

--- src/analytics/load_script.py
+++ src/analytics/load_script.py
@@ -1,4 +1,6 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import ComplementNB
+from sklearn.tree import DecisionTreeClassifier
 
 from src.analytics.model import Model, MultiLabelsClassifier
 from src.analytics.pipeline import TransformPipeline
@@ -6,16 +8,38 @@ from src.analytics.topic.keywords_extractor import KeywordExtractor
 
 
 def load_classifier(folder: str = 'models') -> Model:
-    seed = 7231
+    params1 = {
+        'estimator': DecisionTreeClassifier(max_depth=2,
+                                            min_samples_split=4,
+                                            min_samples_leaf=5),
+        'n_estimators': 940,
+        'learning_rate': 0.054468333976947225,
+        'algorithm': 'SAMME'
+    }
+    relevant_clf = AdaBoostClassifier(**params1)
 
-    clf = MultiLabelsClassifier(RandomForestClassifier(random_state=seed),
-                                RandomForestClassifier(random_state=seed),
-                                RandomForestClassifier(random_state=seed))
+    params2 = {
+        'max_depth': 2,
+        'min_samples_split': 5,
+        'min_samples_leaf': 2,
+        'n_estimators': 480,
+        'learning_rate': 0.008518312708061073
+    }
+    object_clf = GradientBoostingClassifier(**params2)
+    params3 = {
+        'alpha': 1.4420740840383671e-06,
+        'norm': False
+    }
+    positive_clf = ComplementNB(**params3)
+    clf = MultiLabelsClassifier(relevant_clf,
+                                object_clf,
+                                positive_clf)
 
     pipeline = TransformPipeline(columns=['question_1', 'question_2',
                                           'question_3', 'question_4',
                                           'question_5'],
-                                 ngram_range=(1, 2))
+                                 ngram_range=(1, 3),
+                                 max_df=0.9)
 
     model = Model(pipeline, clf)
     return model.load(folder)
