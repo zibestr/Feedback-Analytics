6e820fd253a323c2c2add166bb779310c26fffe6
pyalexos <aligos@yandex.ru>
2024-04-27 03:50:02 +0300

    first view for web. first template and css file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,47 @@
 from flask import Flask, request, render_template, flash, url_for, send_file, session
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 from werkzeug.utils import secure_filename, redirect
-from tools.forms import LoginForm, RegForm
+from tools.forms import LoginForm
 from tools.password_proccessing import hash_password, check_password
 
-from db_models.__all_models import *
+from db_models.courses import Course
+from db_models.feedbacks import Feedback
+from db_models.students import Student
+from db_models.users import User
 from db_models import db_session
 import os
 
 app = Flask(__name__)
-app.secret_key = '379cc9d0797b7d3a445eae49288768c6'
-app.config['SECRET_KEY'] = '379cc9d0797b7d3a445eae49288768c6'
+login_manager = LoginManager(app)
+app.secret_key = 'whgt9jasqzctj24yg79ve5za6jnwfzqg'
+app.config['SECRET_KEY'] = 'whgt9jasqzctj24yg79ve5za6jnwfzqg'
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def homepage():
+    # TODO Здесь описание проекта в шаблоне и форма login
+    return render_template("login.html")
+
+
+@app.route("/me?<id>", methods=["GET", "POST"])
+@login_required
+def me(id):
+    # TODO сюда перенаправляется пользователь после логина, здесь статистика
+    return render_template("index.html")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return session.query(User).get(id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect("/")
 
 
 if __name__ == '__main__':
