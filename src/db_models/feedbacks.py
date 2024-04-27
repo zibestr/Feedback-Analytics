044746cac4065a77c2669384c98029bbb92061fc
adkor <adkorolkov@yandex.ru>
2024-04-27 14:58:49 +0300

    Готовый бот

--- src/db_models/feedbacks.py
+++ src/db_models/feedbacks.py
@@ -7,8 +7,13 @@ from db_models.db_session import SqlAlchemyBase
 class Feedback(SqlAlchemyBase):
     __tablename__ = 'feedbacks'
 
+    
+    __table_args__ = (
+        sqlalchemy.UniqueConstraint('answers', 'student_id'),
+      )
+    
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    answers = sqlalchemy.Column(sqlalchemy.JSON, nullable=False)
+    answers = sqlalchemy.Column(sqlalchemy.String, nullable=False)
 
     student_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("students.id"))
     students = orm.relationship('Student', back_populates='feedbacks')
